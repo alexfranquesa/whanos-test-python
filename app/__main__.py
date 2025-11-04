212f8973c32e61362e2468c331b61a4f26ff34cc
alexfranquesa11 <alexfranquesa11@gmail.com>
2025-11-04 16:03:15 +0100

    fds

--- app/__main__.py
+++ app/__main__.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloIndex():
-    return 'Hello world! This is the main application.'
+    return 'Hello world! This is '
 
 app.run(host='0.0.0.0', port=8080)
